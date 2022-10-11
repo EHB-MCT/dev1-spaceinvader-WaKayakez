610f5cdcbd12888ce712ed5220955a8cf83749fa
Wa Kayakez <wa.kayakez.kayakez@student.ehb.be>
2022-10-11 21:43:49 +0200

    border /canvas

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,37 @@
 "use strict";
 
-alert("Draw your space invader here");
+let canvas = document.querySelector("canvas");
+let context = canvas.getContext("2d");
+
+drawRectangle();
+function drawRectangle(){
+
+    context.beginPath();
+    context.fillStyle="green"
+    context.rect(250,200,50,50)
+    context.fill();
+
+    context.beginPath();
+    context.fillStyle="green"
+    context.rect(200,200,50,50)
+    context.fill();
+
+    context.beginPath();
+    context.fillStyle="green"
+    context.rect(150,200,50,50)
+    context.fill();
+
+    context.beginPath();
+    context.fillStyle="green"
+    context.rect(100,200,50,50)
+    context.fill();
+
+    context.beginPath();
+    context.fillStyle="green"
+    context.rect(50,200,50,50)
+    context.fill();
+
+
+
+}
+
