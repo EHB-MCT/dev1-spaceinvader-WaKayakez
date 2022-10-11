8ff8acad462370a561909cf4f2fc8a6caaed61ca
Wa Kayakez <wa.kayakez.kayakez@student.ehb.be>
2022-10-11 22:36:58 +0200

    changed canvas in CSS

--- spaceInvader.js
+++ spaceInvader.js
@@ -7,29 +7,52 @@ drawRectangle();
 function drawRectangle(){
 
     context.beginPath();
-    context.fillStyle="green"
+    context.fillStyle="#66FF00"
     context.rect(250,200,50,50)
     context.fill();
 
     context.beginPath();
-    context.fillStyle="green"
+    context.fillStyle="#66FF00"
     context.rect(200,200,50,50)
     context.fill();
 
     context.beginPath();
-    context.fillStyle="green"
+    context.fillStyle="#66FF00"
     context.rect(150,200,50,50)
     context.fill();
 
     context.beginPath();
-    context.fillStyle="green"
+    context.fillStyle="66FF00"
     context.rect(100,200,50,50)
     context.fill();
 
     context.beginPath();
-    context.fillStyle="green"
+    context.fillStyle="66FF00"
     context.rect(50,200,50,50)
     context.fill();
+
+    context.beginPath();
+    context.fillStyle="66FF00"
+    context.rect(200,200,50,50)
+    context.fill();
+
+    context.beginPath();
+    context.fillStyle="66FF00"
+    context.rect(200,150,50,50)
+    context.fill();
+
+    context.beginPath();
+    context.fillStyle="66FF00"
+    context.rect(50,150,50,50)
+    context.fill();
+
+    context.beginPath();
+    context.fillStyle="66FF00"
+    context.rect(100,150,50,50)
+    context.fill();
+
+
+
 
 
 
