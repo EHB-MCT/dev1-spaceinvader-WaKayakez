37afa58666964c0ae20aaea5ececbd9f4b8b8a3f
Wa Kayakez <wa.kayakez.kayakez@student.ehb.be>
2022-10-11 23:29:27 +0200

    spaceInvader afgewerkt

--- spaceInvader.js
+++ spaceInvader.js
@@ -6,52 +6,87 @@ let context = canvas.getContext("2d");
 drawRectangle();
 function drawRectangle(){
 
-    context.beginPath();
-    context.fillStyle="#66FF00"
-    context.rect(250,200,50,50)
-    context.fill();
 
     context.beginPath();
     context.fillStyle="#66FF00"
-    context.rect(200,200,50,50)
+    context.rect(20,75,50,25)
     context.fill();
 
     context.beginPath();
     context.fillStyle="#66FF00"
-    context.rect(150,200,50,50)
+    context.rect(20,100,50,25)
     context.fill();
 
     context.beginPath();
-    context.fillStyle="66FF00"
-    context.rect(100,200,50,50)
+    context.fillStyle="#66FF00"
+    context.rect(20,120,50,25)
     context.fill();
 
     context.beginPath();
-    context.fillStyle="66FF00"
-    context.rect(50,200,50,50)
+    context.fillStyle="#66FF00"
+    context.rect(70,120,50,25)
     context.fill();
 
     context.beginPath();
-    context.fillStyle="66FF00"
-    context.rect(200,200,50,50)
+    context.fillStyle="#66FF00"
+    context.rect(120,120,50,25)
     context.fill();
 
     context.beginPath();
-    context.fillStyle="66FF00"
-    context.rect(200,150,50,50)
+    context.fillStyle="#66FF00"
+    context.rect(170,120,50,25)
     context.fill();
 
     context.beginPath();
-    context.fillStyle="66FF00"
-    context.rect(50,150,50,50)
+    context.fillStyle="#66FF00"
+    context.rect(220,120,50,25)
     context.fill();
 
     context.beginPath();
-    context.fillStyle="66FF00"
-    context.rect(100,150,50,50)
+    context.fillStyle="#66FF00"
+    context.rect(170,100,50,25)
     context.fill();
 
+    context.beginPath();
+    context.fillStyle="#66FF00"
+    context.rect(120,75,50,25)
+    context.fill();
 
+    context.beginPath();
+    context.fillStyle="#66FF00"
+    context.rect(220,100,50,25)
+    context.fill();
+
+    
+    context.beginPath();
+    context.fillStyle="#66FF00"
+    context.rect(220,75,50,25)
+    context.fill();
+
+    context.beginPath();
+    context.fillStyle="#66FF00"
+    context.rect(170,100,50,25)
+    context.fill();
+
+    context.beginPath();
+    context.fillStyle="#66FF00"
+    context.rect(70,50,50,25)
+    context.fill();
+
+    context.beginPath();
+    context.fillStyle="#66FF00"
+    context.rect(70,100,50,25)
+    context.fill();
+
+    context.beginPath();
+    context.fillStyle="#66FF00"
+    context.rect(170,50,50,25)
+    context.fill();
+
+    context.beginPath();
+    context.fillStyle="#66FF00"
+    context.rect(120,25,50,25)
+    context.fill();
 
 
 
